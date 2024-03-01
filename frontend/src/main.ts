e0e593b35cfa68e505c26e40bf2d2bd41eadff6c
remikeophila <remi.keophila@gmail.com>
2024-02-29 20:21:17 -0500

    Init Frontend

---
+++ frontend/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app-routing.module';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom([
+      RouterModule.forRoot(routes) // Use your application's routes
+    ])
+  ]
+}).catch(err => console.error(err));
